25b754c04216690a6c99ffcd69784f7d04398a6c
Dawson Richey <dawsonrichey@gmail.com>
2018-06-15 21:43:56 -0400

    unsure changes

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -52,6 +52,13 @@ namespace Treehouse.FitnessFrog.Controllers
 		[HttpPost]
 		public ActionResult Add(Entry entry)
 		{
+			if (ModelState.IsValid)
+			{
+				_entriesRepository.AddEntry(entry);
+
+				// TODO Display the Entries list page
+			}
+
 			return View(entry);
 		}
 
